bb3541abc0038948cbdfd982b29419b08bf96805
reloadedhead <tomasgobet@icloud.com>
2020-07-26 13:06:09 -0300

    add: dark theme support

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useMemo } from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import Router from './Router';
@@ -11,24 +11,43 @@ import axiosConfig from './axios-config';
 import i18n from './i18n';
 import { I18nextProvider } from 'react-i18next';
 import { AlertProvider } from './context/alert';
+import { useMediaQuery, createMuiTheme, MuiThemeProvider } from '@material-ui/core';
 
-const App = () => (
-  <React.StrictMode>
-    <ErrorBoundary FallbackComponent={ErrorFallback}>
-      <Suspense fallback={<LoadingPage />}>
-        <I18nextProvider i18n={i18n}>
-          <AlertProvider>
-            <WeatherProvider>
-              <AppShell>
-                <Router />
-              </AppShell>
-            </WeatherProvider>
-          </AlertProvider>
-        </I18nextProvider>
-      </Suspense>
-    </ErrorBoundary>
-  </React.StrictMode>
-);
+const App = () => {
+  const sysThemePreference = useMediaQuery('(prefers-color-scheme: dark)') ? 'dark' : 'light';
+  const theme = useMemo(
+    () =>
+      createMuiTheme({
+        palette: {
+          primary: {
+            main: '#1976D2',
+          },
+          type: sysThemePreference,
+        },
+      }),
+    [sysThemePreference]
+  );
+
+  return (
+    <React.StrictMode>
+      <MuiThemeProvider theme={theme}>
+        <ErrorBoundary FallbackComponent={ErrorFallback}>
+          <Suspense fallback={<LoadingPage />}>
+            <I18nextProvider i18n={i18n}>
+              <AlertProvider>
+                <WeatherProvider>
+                  <AppShell>
+                    <Router />
+                  </AppShell>
+                </WeatherProvider>
+              </AlertProvider>
+            </I18nextProvider>
+          </Suspense>
+        </ErrorBoundary>
+      </MuiThemeProvider>
+    </React.StrictMode>
+  );
+};
 
 const root = document.getElementById('root');
 if (root !== null) {
